61749bf015e61f271b74c4a63433f11f44350f64
Nick <Nikhilpancholi6@gmail.com>
2015-09-19 01:22:26 +0530

    newversion

--- contact.php
+++ contact.php
@@ -1,16 +1,17 @@
 <?php
-	$to      = 'readsuccessstoiess@gmail.com';
-	$email   = $_POST['email'];
-	$name  = $_POST['name'];
-	$subject = $_POST['subject'];
-	$message = $_POST['comments'];
+$name = $_POST['name'];
+$email = $_POST['email'] ;
+$message = $_POST['comments'] ;
+$subject = $_POST['subject'];
+$body = "Name: $name\r\n";
+$body .= "Message: $message";
 
-	$headers = 'From: http://YourUrl'. '<'.$email.'>' . "\r\n" .
+$to = "readsuccessstoriess@gmail.com";
+$from = "automailer@storyofsuccess.in (Website Automailer)";
+$subject = "Contact Us";
+$headers =  "From: http://storyofsuccess.in/" .'<'.$email.'>' . "\r\n" .
 		'Reply-To: '. $email . "\r\n" .
 		'X-Mailer: PHP/' . phpversion();
-
-	if (mail($to, $subject, $message, $headers)){
-
-	echo "<script>window.location.href = 'http://nikhil16.github.io/nikhil16';</script>";
-   
+$sent = mail($to, $subject, $body, $headers) ;
+echo "<script>window.location.href = 'http://storyofsuccess.in/';</script>";
 ?>
