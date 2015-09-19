162ae964717d1e723b542a5a6c723467a148761a
Nick <Nikhilpancholi6@gmail.com>
2015-09-19 18:53:29 +0530

    asdf

--- contact.php
+++ contact.php
@@ -5,12 +5,13 @@ $message = $_POST['comments'] ;
 $subject = $_POST['subject'];
 
 $to = "readsuccessstoriess@gmail.com";
-//$from = "automailer@storyofsuccess.in (Website Automailer)";
+$from = "automailer@storyofsuccess.in (Website Automailer)";
 $subject = "Contact Us";
 $headers =  "From: $email" .'<'.$email.'>' . "\r\n" .
 		'Reply-To: '. $readsuccessstoriess@gmail.com . "\r\n" .
 		'X-Mailer: PHP/' . phpversion();
 		
-$send = mail($to, $subject, $body, $headers) ;
+//$send = 
+mail($to, $subject, $message, $headers) ;
 header('Location: test.html');
 ?>
