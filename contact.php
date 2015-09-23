7b0b697f192debfe5d0547ebae603080ae33f8cd
Nick <Nikhilpancholi6@gmail.com>
2015-09-23 19:51:41 +0530

    erty

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
-// Check for empty fields
+
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['subject']) 		||
@@ -15,11 +15,10 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 	
-// Create the email and send the message
-$to = 'readsuccessstoriess@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'readsuccessstoriess@gmail.com'; 
 $email_subject = "Website Contact Form:  $subject";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
-$headers = "From: $email\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: $from\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
